0b35642a911b0af70f6aae7cf01f6234f91e1640
pymike00 <pymike00@gmail.com>
2019-03-28 19:03:29 +0100

    Questions App - Serializers

---
+++ QuestionTime/questions/api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from questions.models import Answer, Question
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    created_at = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    user_has_voted = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Answer
+        exclude = ["question", "voters", "updated_at"]
+
+    def get_created_at(self, instance):
+        return instance.created_at.strftime("%B %d %Y")
+
+    def get_likes_count(self, instance):
+        return instance.voters.count()
+
+    def get_user_has_voted(self, instance):
+        request = self.context.get("request")
+        return instance.voters.filter(pk=request.user.pk).exists()
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    created_at = serializers.SerializerMethodField()
+    slug = serializers.SlugField(read_only=True)
+    answers_count = serializers.SerializerMethodField()
+    user_has_answered = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Question
+        exclude = ["updated_at"]
+
+    def get_created_at(self, instance):
+        return instance.created_at.strftime("%B %d %Y")
+
+    def get_answers_count(self, instance):
+        return instance.answers.count()
+
+    def get_user_has_answered(self, instance):
+        request = self.context.get("request")
+        return instance.answers.filter(author=request.user).exists()
