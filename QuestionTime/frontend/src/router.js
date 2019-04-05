08d330ec2b770c8e487db701447b18260bf53f7c
pymike00 <pymike00@gmail.com>
2019-04-05 19:15:06 +0200

    Router Configuration and Example Component

--- QuestionTime/frontend/src/router.js
+++ QuestionTime/frontend/src/router.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Example from "./views/Example.vue";
+import About from "./views/About.vue"
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -14,13 +16,14 @@ export default new Router({
       component: Home
     },
     {
+      path: "/example",
+      name: "example",
+      component: Example
+    },
+    {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
     }
   ]
 });
