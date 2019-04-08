314458cfac79f0fee480a7f78602ac423adcc90a
pymike00 <pymike00@gmail.com>
2019-04-08 18:04:22 +0200

    apiService w/ fetch and csrf_token

---
+++ QuestionTime/frontend/src/common/api.service.js
@@ -0,0 +1,22 @@
+import { CSRF_TOKEN } from "./csrf_token.js"
+
+async function getJSON(response) {
+  if (response.status === 204) return '';
+  return response.json()
+}
+
+function apiService(endpoint, method, data) {
+  const config = {
+    method: method || GET,
+    body: data !== undefined ? JSON.stringify(data) : null,
+    headers: {
+      'content-type': 'application/json',
+      'X-CSRFTOKEN': CSRF_TOKEN
+    }
+  };
+  return fetch(endpoint, config)
+           .then(getJSON)
+           .catch(error => console.log(error))
+}
+
+export { apiService };
