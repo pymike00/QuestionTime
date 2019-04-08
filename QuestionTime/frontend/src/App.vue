c81190d35ee126b23a0a847e4b0969c8994c3be0
pymike00 <pymike00@gmail.com>
2019-04-08 16:14:51 +0200

    Navbar Component

--- QuestionTime/frontend/src/App.vue
+++ QuestionTime/frontend/src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/example">Example</router-link> |
-      <router-link to="/about">About</router-link>      
-    </div>
-    <router-view />
+    <NavbarComponent/>
+    <router-view/>
   </div>
 </template>
 
+<script>
+import NavbarComponent from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    NavbarComponent
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+html,
+body {
+  height: 100%;
+  font-family: "Playfair Display", serif;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.btn:focus {
+  box-shadow: none !important;
 }
 </style>
--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App!!!!!!!!" />
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App!!!!!!!!"/>
+    </div>
   </div>
 </template>
 
