d1551b5b2e5fef5192c16cefc154fb75d0600aa4
pymike00 <pymike00@gmail.com>
2019-04-10 19:11:17 +0200

    Load more Button for Questions in the Homepage

--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div class="container">
+    <div class="container mt-2">
       <div v-for="question in questions"
            :key="question.pk">
         <p class="mb-0">Posted by:
@@ -16,6 +16,15 @@
         <p>Answers: {{ question.answers_count }}</p>
         <hr>
       </div>
+      <div class="my-4">
+        <p v-show="loadingQuestions">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestions"
+          class="btn btn-sm btn-outline-success"
+          >Load More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,16 +35,28 @@ export default {
   name: "home",
   data() {
     return {
-      questions: []
+      questions: [],
+      next: null,
+      loadingQuestions: false
     }
   },
   methods: {
     getQuestions() {
       // make a GET Request to the questions list endpoint and populate the questions array
       let endpoint = "/api/questions/";
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingQuestions = true;
       apiService(endpoint)
         .then(data => {
           this.questions.push(...data.results)
+          this.loadingQuestions = false;
+          if (data.next) {
+            this.next = data.next;
+          } else {
+            this.next = null;
+          }
         })
     }
   },
