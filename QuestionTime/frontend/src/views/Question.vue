d166e6fad399b39e86a3a55613eada441884121f
pymike00 <pymike00@gmail.com>
2019-04-11 17:23:12 +0200

    'Answer the Question' Feature

--- QuestionTime/frontend/src/views/Question.vue
+++ QuestionTime/frontend/src/views/Question.vue
@@ -7,6 +7,36 @@
       </p>
       <p>{{ question.created_at }}</p>
       <hr>
+      <div v-if="userHasAnswered">
+        <p class="answer-added">You've written an answer!</p>
+      </div>
+      <div v-else-if="showForm">
+        <form class="card" @submit.prevent="onSubmit">
+          <div class="card-header px-3">
+            Answer the Question
+          </div>
+          <div class="card-block">
+            <textarea 
+              v-model="newAnswerBody"
+              class="form-control"
+              placeholder="Share Your Knowledge!"
+              rows="5"
+            ></textarea>
+          </div>
+          <div class="card-footer px-3">
+            <button type="submit" class="btn btn-sm btn-success">Submit Your Answer</button>
+          </div>
+        </form>
+        <p v-if="error" class="error mt-2">{{ error }}</p>
+      </div>
+      <div v-else>
+        <button
+          class="btn btn-sm btn-success"
+          @click="showForm = true"
+          >Answer the Question
+        </button>
+      </div>
+      <hr>
     </div>
     <div class="container">
       <AnswerComponent 
@@ -35,7 +65,11 @@ export default {
   data() {
     return {
       question: {},
-      answers: []
+      answers: [],
+      newAnswerBody: null,
+      error: null,
+      userHasAnswered: false,
+      showForm: false
     }
   },
   methods: {
@@ -48,15 +82,35 @@ export default {
       apiService(endpoint)
         .then(data => {
           this.question = data;
+          this.userHasAnswered = data.user_has_answered;
           this.setPageTitle(data.content)
         })
     },
     getQuestionAnswers() {
+      // get a page of answers for a single question from the REST API 
       let endpoint = `/api/questions/${this.slug}/answers/`;
       apiService(endpoint)
         .then(data => {
           this.answers = data.results;
         })
+    },
+    onSubmit() {
+      // submit the content of a new answer to the REST API, then update some data properties
+      if (this.newAnswerBody) {
+        let endpoint = `/api/questions/${this.slug}/answer/`;
+        apiService(endpoint, "POST", { body: this.newAnswerBody })
+          .then(data => {
+            this.answers.unshift(data)
+          })
+        this.newAnswerBody = null;
+        this.showForm = false;
+        this.userHasAnswered = true;
+        if (this.error) {
+          this.error = null;
+        }
+      } else {
+        this.error = "You can't send an empty answer!";
+      }
     }
   },
   created() {
@@ -70,6 +124,16 @@ export default {
 .author-name {
   font-weight: bold;
   color: #DC3545;
+}
+
+.answer-added {
+  font-weight: bold;
+  color: green;
+}
+
+.error {
+  font-weight: bold;
+  color: red; 
 }
 </style>
 
