6fdc6c463458d3c26cdc4ecf416f9e9c307d336c
pymike00 <pymike00@gmail.com>
2019-05-03 11:00:14 +0200

    urllib3 security update

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -169,7 +169,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4 # 10 could be a good value to assign in production. Remember: this currently also applies to answers
+    'PAGE_SIZE': 6 # 10 could be a good value to assign in production. Remember: this currently also applies to answers
 }
 
 WEBPACK_LOADER = {
