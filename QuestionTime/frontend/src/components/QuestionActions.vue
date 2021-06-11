d51e1ec14804ef6f3ecfd1fef66140e2b00b17e1
Michele Saba <pymike00@gmail.com>
2021-06-11 18:26:43 +0200

    REFACTOR: axios implementation for API calls In: QuestionActions.vue (+ code formatting)

--- QuestionTime/frontend/src/components/QuestionActions.vue
+++ QuestionTime/frontend/src/components/QuestionActions.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="question-actions">
     <router-link
-      :to="{name: 'question-editor', params: {slug: slug}}"
+      :to="{ name: 'question-editor', params: { slug: slug } }"
       class="btn btn-sm btn-outline-secondary mr-1"
       >Edit
     </router-link>
-    <button
-      class="btn btn-sm btn-outline-danger"
-      @click="deleteQuestion"
-      >Delete
+    <button class="btn btn-sm btn-outline-danger" @click="deleteQuestion">
+      Delete
     </button>
-  </div>  
+  </div>
 </template>
 
 <script>
-import { apiService } from "@/common/api.service.js";
+import { axios } from "@/common/api.service.js";
 export default {
   name: "QuestionActions",
   props: {
     slug: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
     async deleteQuestion() {
-      let endpoint = `/api/questions/${this.slug}/`;
+      const endpoint = `/api/questions/${this.slug}/`;
       try {
-        await apiService(endpoint, "DELETE");
-        this.$router.push("/");
+        await axios.delete(endpoint);
+        this.$router.push({
+          name: "home",
+        });
+      } catch (error) {
+        console.log(error.response);
+        alert(error.response.statusText);
       }
-      catch (err) {
-        console.log(err);
-      }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
