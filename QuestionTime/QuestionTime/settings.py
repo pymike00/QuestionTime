5371d6d5b8b28845d2614fdd86d31c73a549993b
Michele Saba <pymike00@gmail.com>
2021-07-30 13:01:32 +0200

    REMOVE: 'django.contrib.sites' from installed_apps Which was there because of previously used django-allauth

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
 
     'rest_framework',
     'rest_framework.authtoken',
