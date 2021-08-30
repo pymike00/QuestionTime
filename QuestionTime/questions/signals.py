a9afd6892a67b95de31883f18417b532bdfb3b39
Michele Saba <pymike00@gmail.com>
2021-08-30 16:22:04 +0300

    REFACTOR: add_slug_to_question random extra string Now uses django.utils.crypto.get_random_string

--- QuestionTime/questions/signals.py
+++ QuestionTime/questions/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.utils.crypto import get_random_string
 from django.utils.text import slugify
 
-from core.utils import generate_random_string
 from questions.models import Question
 
 
@@ -10,5 +10,5 @@ from questions.models import Question
 def add_slug_to_question(sender, instance, *args, **kwargs):
     if instance and not instance.slug:
         slug = slugify(instance.content)
-        random_string = generate_random_string()
+        random_string = get_random_string(length=8)
         instance.slug = slug + "-" + random_string
