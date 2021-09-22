6d8bad4a448d342411c7c5a09a55f1338f84eac2
Michele Saba <pymike00@gmail.com>
2021-09-22 12:48:54 +0300

    UPDATE: index.html document.title Now passed via vue.config.js setting

--- QuestionTime/frontend/vue.config.js
+++ QuestionTime/frontend/vue.config.js
@@ -1,22 +1,31 @@
 /*
-Configuration made popular by Ejez and EugeneDae.
-See:
+The django-vue connection approach of which this configuration is part of was made 
+popular by Ejez and EugeneDae. You can find more information about them here:
 https://github.com/django-webpack/django-webpack-loader/issues/209#issue-512863855
 https://github.com/EugeneDae/django-vue-cli-webpack-demo
+
+I however decided to simplify things a little by avoiding template inheritance 
+on Django's side, as this is not needed in our scenario and, I believe, in most
+such cases.
+
+Note: please remember you will also need to properly configure your Django 
+backend for this setup to work as expected both in development and production.
 
 Settings Options:
 https://cli.vuejs.org/config/
 https://v4.webpack.js.org/configuration/dev-server/
-
-Please remember you will also need to properly configure your Django backend
-for this setup to work as expected both in development and production.
 */
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://127.0.0.1:8080',
     outputDir: '../static/dist',
     indexPath: '../../templates/index.html',
-
+    pages: {
+      index: {
+        entry: 'src/main.js',
+        title: 'QuestionTime',
+      },
+    },
     chainWebpack: config => {
         config.devServer
             .public('http://127.0.0.1:8080')
