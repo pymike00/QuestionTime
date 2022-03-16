b8664c098cb35afd6efcf464b4f80a756e4a2742
Michele Saba <pymike00@gmail.com>
2022-03-16 15:13:50 +0100

    UPDATE: vue.config.js

--- QuestionTime/frontend/vue.config.js
+++ QuestionTime/frontend/vue.config.js
@@ -1,44 +1,32 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    // on Windows you might want to set publicPath: "http://127.0.0.1:8080/" 
-    publicPath: "http://0.0.0.0:8080/", 
-    outputDir: './dist/',
+  publicPath: "http://127.0.0.1:8080/",
+  outputDir: "./dist/",
 
-    chainWebpack: config => {
+  chainWebpack: (config) => {
+    config
+      .plugin("BundleTracker")
+      .use(BundleTracker, [{ filename: "./webpack-stats.json" }]);
 
-        config
-            .plugin('BundleTracker')
-            .use(BundleTracker, [{filename: './webpack-stats.json'}])
+    config.output.filename("bundle.js");
+    config.optimization.splitChunks(false);
+    config.resolve.alias.set("__STATIC__", "static");
+  },
 
-        config.output
-            .filename('bundle.js')
-
-        config.optimization
-        	.splitChunks(false)
-
-        config.resolve.alias
-            .set('__STATIC__', 'static')
-
-        config.devServer
-            // the first 3 lines of the following code have been added to the configuration
-            .public('http://127.0.0.1:8080')    
-            .host('127.0.0.1')    
-            .port(8080)
-            .hotOnly(true)
-            .watchOptions({poll: 1000})
-            .https(false)
-            .disableHostCheck(true)
-            .headers({"Access-Control-Allow-Origin": ["\*"]})
-
+  devServer: {
+    devMiddleware: {
+      publicPath: "http://127.0.0.1:8080",
     },
+    hot: "only",
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
 
-    // uncomment before executing 'npm run build' 
-    // css: {
-    //     extract: {
-    //       filename: 'bundle.css',
-    //       chunkFilename: 'bundle.css',
-    //     },
-    // }
-
+//   uncomment before executing 'npm run build' if you wish to have a separate .css file
+//   css: {
+//       extract: {
+//         filename: 'bundle.css',
+//         chunkFilename: 'bundle.css',
+//       },
+//   }
 };
